0ad5acadfc44e5926fcf12a2e4a269cf94fe4564
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-17 23:37:22 +0530

    first commit

--- components/Trading_down.js
+++ components/Trading_down.js
@@ -42,11 +42,8 @@ export default function Trading_down() {
               ))}
             </TextField>
 
-<<<<<<< HEAD
             <button type="submit" className="text-white h-max ml-7 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">GO...</button>
-=======
-            <button type="submit" className="text-white h-max mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">GO...</button>
->>>>>>> 5dc1613fe242448a1574d520e794361c52d8e3ae
+
         </div>
     )
 }
