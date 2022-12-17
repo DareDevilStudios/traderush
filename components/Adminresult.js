87e435e7a2ea95dee96587865c46b933bfd395c9
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-18 03:58:34 +0530

    first commit

--- components/Adminresult.js
+++ components/Adminresult.js
@@ -29,6 +29,18 @@ export default function Adminresult() {
         },
       ];
 
+      const insights = () => {
+        const check = localStorage.getItem('user_data')
+        if(check === Data) {
+          localStorage.setItem("result",true)
+        }
+        else
+        {
+          localStorage.setItem("result",false)
+          localStorage.setItem("insights",Text)
+        }
+      }
+
     return (
         <div class="w-100 flex">
             <div class="w-50 rounded-md flex flex-col p-5 justify-center mt-9">
@@ -41,7 +53,7 @@ export default function Adminresult() {
                     label="Select"
                     defaultValue="EUR"
                     helperText="Selected Currency"
-                    // onChange={(e)=> {}}
+                    onChange={(e)=> {setData(e.target.value)}}
                   >
                     {currencies.map((option) => (
                       <MenuItem key={option.value} value={option.value}>
@@ -50,14 +62,14 @@ export default function Adminresult() {
                     ))}
                   </TextField>
 
-                      <button type="submit" className="text-white h-max mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">GO...</button>
+                      
                 </div>
                 <div class="mt-10">
               
                     <form action="">
-                      <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Insights</label>
-                      <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Why this?..."></textarea>
-                      <button type="submit" className="mt-5 text-white h-max mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                      <label for="message" class="block mb-2 font-medium text-gray-900 dark:text-dark text-2xl">Insights</label>
+                      <textarea value={Text} onChange={(e)=> {setText(e.target.value)}} id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Why this?..."></textarea>
+                      <button onClick={()=> insights()} type="button" className="mt-5 text-white h-max mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     </form>
 
                 </div>
