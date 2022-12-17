51e753267ed1aec69deaa8a794efebdbd38e8e18
muhammed rinshad s r <93196049+daredevilstudio@users.noreply.github.com>
2022-12-17 18:03:09 +0530

    first commit

--- components/Trading_down.js
+++ components/Trading_down.js
@@ -7,9 +7,9 @@ export default function Trading_down() {
             <h1 className="p-3 mr-4">Select a Graph which is going to be next </h1>
             
             <div className="flex flex-col">
-                <button onClick={() => this.setHidden(!this.isHidden)} id="dropdownDefault" data-dropdown-toggle="#dropdown" className="text-white h-max mr-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                <button onClick={() => {setHidden(!isHidden)}} id="dropdownDefault" data-dropdown-toggle="#dropdown" className="text-white h-max mr-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
                 
-                <div id="dropdown" className= "hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+                <div id="dropdown" className= {isHidden}>
                     <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                         <li>
                             <a href="#" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
